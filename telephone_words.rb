09479f42a369cc4fa0a7c044ae3446ae9049e45b
Jason Corum <jason.corum@gmail.com>
2015-10-25 12:00:28 -0400

    Added join method

--- telephone_words.rb
+++ telephone_words.rb
@@ -9,7 +9,7 @@
 # 	AJ, AK, AL, BJ, BK, BL, CJ, CK, CL
 
 
-def run	
+def telephoneWords( number_array )
 	two 	= [ "A", "B", "C" ]
 	three = [ "D", "E", "F" ]
 	four 	= [ "G", "H", "I" ]
@@ -21,28 +21,23 @@ def run
 
 	parsed_numbers = []
 
-	puts "Please enter a some numbers to translate"
-	number_array = gets.strip.chomp
-	
-	number_array = number_array.split(//)
-
 	number_array.each do | num |
 		case num
-		when "2"
+		when 2
 			parsed_numbers << two
-		when "3"
+		when 3
 			parsed_numbers << three
-		when "4"
+		when 4
 			parsed_numbers << four
-		when "5"
+		when 5
 			parsed_numbers << five
-		when "6"
+		when 6
 			parsed_numbers << six
-		when "7"
+		when 7
 			parsed_numbers << seven
-		when "8"
+		when 8
 			parsed_numbers << eight
-		when "9"
+		when 9
 			parsed_numbers << nine			
 		end		
 	end
@@ -50,8 +45,10 @@ def run
 	starting_array = parsed_numbers[0]
 	parsed_numbers.shift.flatten
 
-	puts "Combos: #{starting_array.product(*parsed_numbers)}"
-
+	final_array = starting_array.product(*parsed_numbers)
+	final_array.each do | response |
+		puts response.join
+	end
 end
 
-run
+telephoneWords( [ 2,5 ])
